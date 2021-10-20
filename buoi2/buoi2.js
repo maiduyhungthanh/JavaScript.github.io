ccc336de28dfcb42e359064bcdeab7b6c1613753
maiduyhungthanh@gmail.com <maiduyhungthanh@gmail.com>
2021-10-20 14:21:20 +0700

    nộp bài 2

--- buoi2/buoi2.js
+++ buoi2/buoi2.js
@@ -1,12 +1,4 @@
-function practice07(arr) {
-    let newArr = []
-    for (let i = 0; i < arr.length; i++) {
-        newArr.push(arr[i] % 2)
-    }
-    return newArr
-}
-console.log(practice07([1, 2, 3, 4, 4]))
-
+// Array
 //bài 1
 function checkElementExist(n,nums){
     let mySet = new Set(nums);
@@ -29,6 +21,13 @@ function max2Number (nums){
 console.log(max2Number([1,2,3,5,5]))
 
 //bài 3
+function ramdomElement(arr){
+    let n = arr[Math.floor(Math.random() * arr.length)];
+    return n;
+}
+console.log(ramdomElement([1,2,3]));
+
+//bài 4
 function findMaxLengthElement(st){
     let maxChar = [];
     maxChar.push(st[0]);
@@ -44,8 +43,75 @@ function findMaxLengthElement(st){
     return maxChar;
 }
 console.log(findMaxLengthElement(['aa','bb','ccc','ddd']))
+//bài 5
 
-//bài 4
+function Similarity(arr1, arr2) {
+	const result = [];
+	arr1.forEach((n) => {
+		if (arr2.includes(n)) {
+			result.push(n);
+		}
+	});
+	return result;
+}
+console.log(Similarity([1,2,3],[1,2,4]))
+//bài 6
+function removeDuplicate(arr) {
+	const result = [];
+	arr.forEach((n) => {
+		if (!result.includes(n)) {
+			result.push(n);
+		}
+	});
+	return result;
+}
+console.log(removeDuplicate([1,2,2,2,3,3,4]));
+//bài 7
+function randomHexCode() {
+    const letters = '0123456789abcdef';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+    }
+console.log(randomHexCode());
+
+//bài 8
+function randomRgbCode(){
+    const nums=[];
+    for (let i=0;i<256;i++){
+        nums.push(i);
+    }
+    let rgb1 = nums[Math.floor(Math.random() * 256)];
+    let rgb2 = nums[Math.floor(Math.random() * 256)];
+    let rgb3 = nums[Math.floor(Math.random() * 256)];
+    return "rgb(" +rgb1+","+rgb2+","+rgb3+ ")";
+}
+console.log(randomRgbCode());
+
+//String
+//bài 1
+function checkStringExist(str1,str2){
+    for (let i=str2.length-1;i>=0;i--){
+        if(str2.endsWith(str1,i+1)){
+            return true;
+        }
+    }
+    return false
+}
+console.log(checkStringExist('chào','xin chào mọi người'));
+
+//bài 2
+function shortenString(str){
+    if(str.length<9)
+    return str;
+    let s = str.slice(0,8)
+    return s+'...';
+}
+console.log(shortenString('xin chào mọi người'));
+
+//bài 3
 function capitalizeString(st){
     st = st.toLocaleLowerCase();
     
@@ -56,6 +122,16 @@ function capitalizeString(st){
 return s.join(" ");
 }
 console.log(capitalizeString('chÀo MừnG đẾn với techMaster'));
+
+//bài 4
+function repeatString(str,n){
+    let s=[];
+ for (let i=0;i<n;i++){
+     s.push(str);
+ }
+ return s.join('-');
+}
+console.log(repeatString('a',5));
 
 //bài 5
 function reverseString(st){
@@ -77,5 +153,67 @@ function checkSymmetricString(st){
     return true;
     return false;
 }
-console.log(checkSymmetricString('Race car'))
+console.log(checkSymmetricString('Race car'));
 
+//bài 7
+function countNumberVowels(str){
+    const s= ['a','o','e','u','i'];
+    let count =0;
+    let st =str.split('');
+    st.forEach(element => {
+        if (s.includes(element)) {
+			count ++;
+		}
+    });
+    return count;
+}
+console.log(countNumberVowels('hello hien'));
+
+//bài 8
+function confirmEnding(st1,st2){
+   return st1.endsWith(st2);
+}
+console.log(confirmEnding('hello','lo'));
+
+//bài 9
+function getFirstLetter(st){
+    const s= st.split(' ');
+    let n = [];
+    for (let i=0;i<s.length;i++){
+        n.push(s[i][0]);
+    }
+    return n.join(' ');
+}
+console.log(getFirstLetter('xin chao moi nguoi'));
+
+//bài 10
+function  sortLetters(st) {
+    st = st.replace(/\s+/g, '');
+    const s= st.split('').sort();
+    return s.join('');
+}
+console.log(sortLetters('xIn chaO mOi ngUoi'));
+
+//bài 11
+function getLetterNoRepeat(st){
+    st =st.replace(/\s+/g, '');
+    const s= st.split('');
+    let myMap = new Map();
+    let result = [];
+    for (let i=0;i<s.length;i++){
+        if(!myMap.has(s[i])){
+            myMap.set(s[i],1);
+        }else{
+            let soLanLap = myMap.get(s[i]);
+                soLanLap++;
+                myMap.set(s[i], soLanLap);
+        }
+    }
+    for (const [key, value] of myMap) {
+       if (value ==1){
+        result.push(key);
+       }
+      }
+    return result.join();
+}
+console.log(getLetterNoRepeat('aaaabcdd'));
